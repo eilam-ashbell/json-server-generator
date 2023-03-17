6136ab4c35097f8d47f5e0b4da4c8dfd7304a011
eilam-ashbell <eashbell@gmail.com>
2023-03-17 16:48:05 +0200

    convert app.ts to intex.ts and class oriented

--- src/schemas.ts
+++ src/schemas.ts
@@ -1,17 +1,26 @@
-import { faker } from '@faker-js/faker';
-import generate from './modules/generators';
- 
+import { faker } from "@faker-js/faker";
+import generate from "./modules/generators";
+
 // data schema for example:
 export class student {
     static "amount" = 5;
-    static "defaultId" = true
-    constructor() {
-        this["gender"] = generate.person.gender()
-        this["fName"] = generate.person.firstName()
-        this["lName"] = generate.person.lastName()
-        this["age"] = generate.random.number(10, 100)
-        this["image"] = faker.image.avatar()
-        this["phone"] = generate.phone.number(generate.phone.countryCode() + "-###-###-####")
-        this["email"] = faker.internet.email(this["fName"], this["lName"])
-    }
-};
+    static "defaultId" = true;
+    public "gender" = generate.person.gender();
+    public "fName" = generate.person.firstName();
+    public "lName" = generate.person.lastName();
+    public "age" = generate.random.number(10, 100);
+    public "image" = faker.image.avatar();
+    public "email" = faker.internet.email(this["fName"], this["lName"]);
+    public "phone" = generate.phone.number(
+        generate.phone.countryCode() + "-###-###-####"
+    );
+    // constructor() {
+    //     this["gender"] = generate.person.gender()
+    //     this["fName"] = generate.person.firstName()
+    //     this["lName"] = generate.person.lastName()
+    //     this["age"] = generate.random.number(10, 100)
+    //     this["image"] = faker.image.avatar()
+    //     this["phone"] = generate.phone.number(generate.phone.countryCode() + "-###-###-####")
+    //     this["email"] = faker.internet.email(this["fName"], this["lName"])
+    // }
+}
